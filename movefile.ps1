d02bf1839eca0fac4b2e65fe1aacb958bca19707
kondekiran <110444026+kondekiran@users.noreply.github.com>
2023-04-03 17:48:59 +0530

    Update movefile.ps1

--- movefile.ps1
+++ movefile.ps1
@@ -1,5 +1,5 @@
 # Set variables
-$githubRepo = "https://github.com/username/repo.git"
+$githubRepo = "https://github.com/kondekiran/frantend.git"
 $awsRepo = "https://git-codecommit.us-east-1.amazonaws.com/v1/repos/FrontEnd-Angular"
 $accessKeyId = $env:AWS_ACCESS_KEY_ID
 $secretAccessKey = $env:AWS_SECRET_ACCESS_KEY
